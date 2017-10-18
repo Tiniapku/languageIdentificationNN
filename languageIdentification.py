b8ebf011d811eec085f9300c98601c39efb6fb2c
Tinia Liu <liutianyipku@gmail.com>
2017-10-18 01:25:07 -0400

    Updated version
    Including test features when transforming. Cleaner code

--- languageIdentification.py
+++ languageIdentification.py
@@ -1,19 +1,20 @@
-# -*- coding: utf-8 -*-
 import sys
 from random import randint
 from collections import Counter
 from sklearn import preprocessing
 from sklearn.preprocessing.label import LabelBinarizer
-from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score
 import numpy as np
 import matplotlib.pyplot as pl
+
 np.set_printoptions(threshold=np.nan)
+
 class languageIdentification(object):
     """
     Using characters as features, each encoded by sklearn OneHotEncoder
     Languages are encoded into vectors using sklearn LabelBinarizer
     """
-    def __init__(self, trainFile, d = 100, yita = 0.1):
+    def __init__(self, trainFile, devFile, testFile, d = 100, yita = 0.1):
         self.d = d
         self.yita = yita
         self.languages = {"ENGLISH": 1, "FRENCH": 3, "ITALIAN": 2}
@@ -21,14 +22,16 @@ class languageIdentification(object):
                              "[", "]", "|", "<", ">", "+", "=", "@", "#", "$", "%", "^","&", "*"]
         self.noPunctuation = False
         self.answerLables = LabelBinarizer()
-
+        self.answerLables.fit([1, 2, 3])
         self.c = set()
-        self.trainLabels = preprocessing.LabelEncoder()
-        self.rawResult = []
-        self.train, self.result, self.inputlen = self.trainProcessing(trainFile)
-        self.v = preprocessing.OneHotEncoder(n_values=self.inputlen)
-        self.train = self.v.fit_transform(self.train).toarray()
-        self.input = self.inputlen * 5 + 1
+        #self.trainLabels = preprocessing.LabelEncoder()
+        #self.rawResult = []
+        #self.train, self.result, self.inputlen = self.trainProcessing(trainFile)
+        #self.v = preprocessing.OneHotEncoder(n_values=self.inputlen)
+        #self.train = self.v.fit_transform(self.train).toarray()
+        self.Initialize(trainFile, devFile, testFile)
+
+        self.input = len(self.c) * 5 + 1
         self.hidden = d
         self.output = 3
 
@@ -45,13 +48,10 @@ class languageIdentification(object):
     def trainProcessing(self, trainFile):
         trainList = []
         result = []
-        #self.answerLables.fit(["ENGLISH", "FRENCH", "ITALIAN"])
         self.answerLables.fit([1,2,3])
-        #print self.answerLables.classes_
         for line in trainFile:
-            #print line
             space = line.index(" ")
-            answer, train = line[:space], line[space + 1:]
+            answer, train = line[:space].upper(), line[space + 1:]
             li, ans = self.lineProc(train, answer, True)
             trainList += li
             result += ans
@@ -60,42 +60,136 @@ class languageIdentification(object):
         trainList = np.array(trainList)
         trainList = self.trainLabels.fit_transform(trainList.ravel()).reshape(*trainList.shape)
         result = self.answerLables.fit_transform(result)
-        #print self.trainLabels.classes_
         inputLen = len(list(self.trainLabels.classes_))
-        #print trainList.shape
-        #print inputLen
-        #print self.v.n_values
-        #print self.v.feature_indices_
-        #print result
-        #print trainList
+
         return (trainList, result, inputLen)
+
+    def Initialize(self, trainFile, devFile, testFile):
+        trainList = []
+        result = []
+        testList = []
+        devList = []
+        devResult = []
+        rawResult = []
+        for line in trainFile:
+            space = line.index(" ")
+            answer, train = line[:space].upper(), line[space + 1:]
+            li, ans = self.lineProc(train, answer, True)
+            trainList += li
+            result += ans
+            rawResult.append(answer)
+        trainList, result = self.FisherYatesShuffle(trainList, result)
+        self.trainResult = result
+        self.rawResult = rawResult
+        self.trainResult = self.answerLables.fit_transform(result)
+
+        for line in devFile:
+            space = line.index(" ")
+            answer, train = line[:space].upper(), line[space + 1:]
+            li, ans = self.lineProc(train, answer, True)
+            devList += li
+            devResult += ans
+        self.devResult = devResult
+
+        for line in testFile:
+            test = self.lineProc(line, "", False)
+            testList.append(test)
+
+        self.trainLabels = preprocessing.LabelEncoder()
+        self.trainLabels.fit(list(self.c))
+        self.train = self.trainLabels.transform(trainList.ravel()).reshape(*trainList.shape)
+        self.dev = self.trainLabels.transform(devList.ravel()).reshape(*devList.shape)
+        self.test = self.trainLabels.transform(testList.ravel()).reshape(*devList.shape)
+
+        self.v = OneHotEncoder()
+
+        self.train = self.v.fit_transform(self.train).toarray()
+        self.dev = self.v.transform(self.dev).toarray()
+        self.test = self.v.transform(self.test).toarray()
+
+    def devProcess(self):
+        trainAccuracy = []
+        devAccuracy = []
+
+        print "initial predictions"
+        train_prediction = self.predictAll(self.train)
+        initial_train = self.evaluate(train_prediction, self.rawResult)
+        trainAccuracy.append(initial_train)
+        print "initial train accuracy: ", initial_train
+
+        dev_prediction = self.predictAll(self.dev)
+        initial_dev = self.evaluate(dev_prediction, self.devResult)
+        devAccuracy.append(initial_dev)
+        print "initial dev accuracy: ", initial_dev
+
+        for i in xrange(epoch):
+            print "************************************epoch:", i + 1, "************************************"
+            trainac, err = self.trainNN(1)
+            print "train accuracy:", trainac
+            trainAccuracy.append(trainac)
+            dev_prediction = self.predictAll(self.dev)
+            devac = self.evaluate(dev_prediction, self.devResult)
+            print "dev accuracy:", devac
+            devAccuracy.append(devac)
+        x = [0,1,2,3]
+        pl.plot(x, trainAccuracy, 'r--', x, devAccuracy, 'bs')
+        pl.show()
+
+    def getTestResult(self):
+        test_results = open('languageIdentification.data/test_solutions', 'r')
+        golden = []
+        for line in test_results.readlines():
+            golden.append(solution.languages[line.strip().split(" ")[1].upper()])
+
+        return golden
+
+    def testProcess(self, golden):
+        test_prediction = self.predictAll(self.test)
+        testac = self.evaluate(test_prediction, golden)
+        print "test accuracy: ", testac
+
+    def resetParameters(self, d, yita):
+        self.d = d
+        self.yita = yita
+        self.hidden = d
+
+        self.ai = np.array([1.0] * self.input)
+        self.ah = np.array([1.0] * (self.hidden + 1))
+        self.ao = [1.0] * self.output
+
+        self.wi = np.random.uniform(size = (self.input, self.hidden))
+        self.wo= np.random.randn(self.hidden + 1, self.output)
+
+        self.ci = np.zeros((self.input, self.hidden))
+        self.co = np.zeros((self.hidden + 1, self.output))
 
     def testProcessing(self, testFile):
         prediction_results = []
         for line in testFile:
-            test = self.lineProc(line,"", False)
-            #for i in xrange(len(test)):
+            test = self.testLineProc(line)
             test = np.array(test)
             test = self.trainLabels.transform(test.ravel()).reshape(*test.shape)
-            #print test
             test = self.v.transform(test).toarray()
-            #print res
-            #print res.shape
             res = self.predict(test)
-            #print line, res
             prediction_results.append(res)
 
 
         return prediction_results
 
+    def updateParameter(self, d, yita):
+        self.d = d
+        self.yita 
+
+        = yita
 
     def lineProc(self, line, answer, isTraining = True):
-        #line = encode(line)
         text = []
         result = []
         for ch in line:
             self.c.add(ch)
-        for i in xrange(len(line) - 5):
+        if len(line) < 5:
+            line += " " * (5 - len(line))
+        for i in xrange(len(line) - 4):
             text.append(list(line[i : i + 5]))
             if isTraining:
                 result.append(self.languages[answer])
@@ -103,6 +197,15 @@ class languageIdentification(object):
             return (text, result)
         else:
             return text
+
+    def testLineProc(self, line):
+        text = []
+        #line = line.replace(u'\xf3', "")
+        if len(line) <= 5:
+            line += " " * (5 - len(line))
+        for i in xrange(len(line) - 4):
+            text.append(list(line[i : i + 5]))
+        return text
 
     def FisherYatesShuffle(self, train, result):
         l = len(train)
@@ -214,21 +317,15 @@ class languageIdentification(object):
         prediction_results = []
         for line in testFile:
             space = line.index(" ")
-            answer, train = line[:space], line[space + 1:]
+            answer, train = line[:space].upper(), line[space + 1:]
             #print answer, train
             test_results.append(self.languages[answer.strip()])
-            test = self.lineProc(train,"", False)
-            #for i in xrange(len(test)):
+            test = self.testLineProc(train)
             test = np.array(test)
             test = self.trainLabels.transform(test.ravel()).reshape(*test.shape)
-            #print test
             test = self.v.transform(test).toarray()
-            #print res
-            #print res.shape
             res = self.predict(test)
-            #print line, res
             prediction_results.append(res)
-
 
         return prediction_results, test_results
 
@@ -237,19 +334,17 @@ class languageIdentification(object):
         results = []
         for line in content:
             space = line.index(" ")
-            answer, train = line[:space], line[space + 1:]
-            #print answer, train
+            answer, train = line[:space].upper(), line[space + 1:]
             results.append(self.languages[answer.strip()])
-            test = self.lineProc(train,"", False)
-            #for i in xrange(len(test)):
+            test = self.testLineProc(train)
             test = np.array(test)
             test = self.trainLabels.transform(test.ravel()).reshape(*test.shape)
             test = self.v.transform(test).toarray()
             features.append(test)
-
+        #print "Features: ", len(features), len(results)
         return features, results
 
-    def predictFeatures(self, features):
+    def predictAll(self, features):
         predict_result = []
         for f in features:
             res = self.predict(f)
@@ -260,6 +355,7 @@ def devProcess(trainFilename, devFileName, epoch = 3):
     trainAccuracy = []
     devAccuracy = []
 
+    print "read in files"
     ftrain = open(trainFilename, 'r')
     train = ftrain.read().strip().split("\n")
     ftrain.close()
@@ -270,6 +366,7 @@ def devProcess(trainFilename, devFileName, epoch = 3):
     dev = fdev.read().strip().split("\n")
     fdev.close()
 
+    print "initial predictions"
     train_features, train_results = solution.getFileFeatures(train)
     train_prediction = solution.predictFeatures(train_features)
     initial_train = solution.evaluate(train_prediction, train_results)
@@ -299,26 +396,56 @@ if __name__ == "__main__":
     trainFileName = sys.argv[1]
     devFileName = sys.argv[2]
     testFileName = sys.argv[3]
-    resultFileName = sys.argv[4]
-    devProcess(trainFileName, devFileName)
+
+    ftrain = open(trainFileName, 'r')
+    train = ftrain.read().decode('utf-8').strip().split("\n")
+
+    fdev = open(devFileName, 'r')
+    dev = fdev.read().decode('utf-8').strip().split("\n")
+
+    ftest = open(testFileName, 'r')
+    test = ftest.read().decode('latin-1').strip().split("\n")
+
+    solution = languageIdentification(train, dev, test, 100, 0.1)
+    print "************************************Part 0: dev data result************************************"
+    solution.devProcess(trainFileName, devFileName)
+
+    print "************************************Part 1: test data result************************************"
+
+    golden = solution.getTestResult()
+
+    solution.testProcess(golden)
 
     """
     ftrain = open(trainFileName, 'r')
-    solution = languageIdentification(ftrain, 3, 0.1)
-    #print solution.result
-    #print solution.wi
+    train = ftrain.read().decode('utf-8').strip().split("\n")
+    solution = languageIdentification(train, 100, 0.1)
     ftrain.close()
-    print "************************************train************************************"
+
+    print "training..."
     solution.trainNN(3)
-    #print solution.wi
     ftest = open(testFileName, 'r')
-    print "************************************test************************************"
-    predictions = solution.testProcessing(ftest)
-    print predictions
-    test_results = open(resultFileName, 'r')
+    test = ftest.read().decode('latin-1').strip().split("\n")
+
+    print "testing..."
+    predictions = solution.testProcessing(test)
+    test_results = open('languageIdentification.data/test_solutions', 'r')
     golden = []
     for line in test_results.readlines():
-        golden.append(solution.languages[line.strip()])
-    solution.evaluate(predictions, golden)
+        golden.append(solution.languages[line.strip().split(" ")[1].upper()])
+    print predictions
+    print golden
+    accuracy = solution.evaluate(predictions, golden)
+    print accuracy
     """
+    print "************************************Part2: hyperparameter optimization************************************"
 
+    d_list = [50, 80, 100, 120, 150]
+    yita_list = [0.03, 0.1, 0.3, 1, 3]
+
+    for d in d_list:
+        for yita in yita_list:
+            solution.updateParameter(d, yita)
+            solution.trainNN(1)
+            print "The test result of hyperparameter d:", d, "and yita:", yita
+            solution.testProcess(golden)
